8574c17da5d4485500c63af26b8012ecd4b451b2
pedrofavarao <pedro.favarao@socin.com.br>
2024-08-03 17:53:05 -0300

    initial commit

---
+++ src/main/java/com/citel/vital_web_api/VitalWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.citel.vital_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalWebApiApplication.class, args);
+	}
+
+}
