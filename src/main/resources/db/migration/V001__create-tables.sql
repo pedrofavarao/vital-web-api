395f76d4ad299801f1c025d14a41772df797ccc3
pedrofavarao <pedro.favarao@socin.com.br>
2024-08-04 00:46:57 -0300

    feature: Adicionado fyway migration para criar e popular tabelas

---
+++ src/main/resources/db/migration/V001__create-tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE donators (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    age INTEGER NOT NULL,
+    height INTEGER NOT NULL,
+    weight INTEGER NOT NULL,
+    blood_type VARCHAR(4) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE address (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    city VARCHAR(255),
+    state VARCHAR(255),
+    PRIMARY KEY (id)
+);
